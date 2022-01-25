f08a6535b779268760a5bf6e9b4f6452b326eadb
Tomi Poppe <tomas.poppe@gmail.com>
2022-01-25 16:19:17 -0300

    Modificaciones en el media queries

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 });   
 ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards', {delay: 400});
-ScrollReveal().reveal('.card-banner-one', {delay: 500});
-ScrollReveal().reveal('.card-banner-two', {delay: 500});
+ScrollReveal().reveal('.news-cards', {delay: 100});
+ScrollReveal().reveal('.card-banner-one', {delay: 100});
+ScrollReveal().reveal('.card-banner-two', {delay: 100});
