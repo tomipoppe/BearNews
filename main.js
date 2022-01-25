212e24e0afa5483f131ffc6542d2acaa0122a216
Tomi Poppe <tomas.poppe@gmail.com>
2022-01-24 21:07:20 -0300

    Este es el primer commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});   
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 400});
+ScrollReveal().reveal('.card-banner-one', {delay: 500});
+ScrollReveal().reveal('.card-banner-two', {delay: 500});
